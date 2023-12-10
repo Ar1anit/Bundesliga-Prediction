b47e9c5fa3b381ba50c0a7f0de9d4155845fddbb
irfan <irfanyilmazer@stud.uni-due.de>
2023-12-10 19:46:03 +0100

    New requriements.txt and first scraper structure

--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
 # Create your views here.
+# In Ihrer views.py
+
+def scrape_bundesliga(request):
+    url = 'https://www.bundesliga.com/de/bundesliga/spieltag'
+    # Beispiel: Daten von einer Webseite abrufen und verarbeiten
+
+    return HttpResponse("Scraping erfolgreich durchgeführt")
